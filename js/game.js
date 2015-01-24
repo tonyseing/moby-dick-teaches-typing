5ab9899565cb7acab0676ce6e32c34ef4b899a3a
tony seing <tony.seing@gmail.com>
2015-01-23 20:02:01 -0500

    adds counter log and uses window to track key presses

--- js/game.js
+++ js/game.js
@@ -1,32 +1,54 @@
 "use strict"
 
-// declare the Game prototype
-function   Game(book, progress) {
-  this.book = { title: book, text: "" };
-  this.progress = progress; // point in text that player has reached
-  this.fetch_book = function() {
-    return Bacon.fromPromise($.get(this.book.title));
-  }.bind(this);
-}
+var app = app || {};
 
-// initialize a game
-var game = new Game("texts/mobydick.txt", 0);
+app.game = (function() {
+    function ajax(url) { 
+        return Bacon.fromPromise($.get(url));
+    }
 
-var book_stream = game.fetch_book();
+    // declare the Game prototype
+    function Game(book, progress) {
+        this.book = { title: book, text: "" };
+        this.progress = progress; // point in text that player has reached
+        this.fetch_book = function() {
+            return ajax(this.book.title);
+        }.bind(this);
+    }
 
-var chunk_stream = book_stream
-    .flatMap(function(book_text) {
-      return book_text.substring(game.progress, game.progress + 100).split("");
-    }).log();
+    // initialize a game
+    var game = new Game("texts/mobydick.txt", 0);
+    var book_stream = game.fetch_book();
+    var chunk_stream = book_stream
+        .map(function(book_text) {
+            return book_text.substring(game.progress, game.progress + 500).split("");
+        });
+    // cursor always starts at 0
+    var cursor_location = 0;
+    
+    // creates a stream that emits the value 1 every second
+    var seconds_stream = Bacon.interval(1000, 1);
+    seconds_stream.onValue(function(val) {
+        console.log(val);
+    });
+
+    chunk_stream.onValue(function(val) { 
+        $(".text-target").html(val);
+    });
 
 
-
-chunk_stream.toProperty().assign($(".text-target"), "html");
-
-$(document).ready(function() {
-  var typed_keys = $(".text-typed")
-      .asEventStream("keypress")
-      .map(function(event) { return event.target.value; }).merge(chunk_stream).log();
-  
-
-});
+    $(document).ready(function() {
+        var typed_keys = $(window)
+            .asEventStream("keypress")
+            .map(function(event) { 
+                $(".text-typed").val("");
+                return String.fromCharCode(event.keyCode || event.which); 
+            });
+        cursor_location = typed_keys.map(1).scan(0, function(a,b) { 
+            return a + b; 
+        });
+        cursor_location.onValue(function(val) { 
+            console.log(val);
+        });
+    });
+})();
